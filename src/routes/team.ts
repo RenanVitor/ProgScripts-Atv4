17303ee090627e7dd46e485de7b365504702ef38
Renan Vitor <56437836+RenanVitor@users.noreply.github.com>
2023-05-30 00:22:59 -0300

    Add files via upload

---
+++ src/routes/team.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import TeamController from "../controllers/TeamController";
+
+const routes = Router();
+
+routes.get('/', TeamController.getAllTeam);
+routes.get('/:termo', TeamController.getTermo);
+routes.post('/create', TeamController.postTeam);
+routes.put('/update/:', TeamController.putTeam);
+routes.delete('/:', TeamController.deleteTeam);
+
+export default routes;
